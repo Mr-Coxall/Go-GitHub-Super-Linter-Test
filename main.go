45f28e690fd704210e60a9a28c92a1cc38f0f5ec
Patrick Coxall <mr.coxall@mths.ca>
2025-04-25 15:17:47 +0000

    init

---
+++ main.go
@@ -0,0 +1,13 @@
+/**
+ * Created by: Mr. Coxall
+ * Created on: Sep 2020
+ * This is a simple Go program that prints "Hello, World!" to the console
+ */
+package main
+
+import "fmt"
+
+func main() {
+	// Print "Hello, World!" to the console
+	fmt.Println("Hello, World!")
+}
